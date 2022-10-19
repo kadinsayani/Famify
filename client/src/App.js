71a7bcef45fbcad9fe53d67376e7d13bd74ebe0a
Kadin Sayani <kadinsayani8@gmail.com>
2022-10-19 10:46:04 -0600

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+ 
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+};
+
+export default App;
