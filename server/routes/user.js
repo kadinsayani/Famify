a7f8528e7c63b3a9367c3f7ad6f98270089ed118
Leinard Grande <leinard.grande@ucalgary.ca>
2022-11-09 16:01:24 -0700

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,14 +1,10 @@
 const express = require("express");
-
-// recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /record.
 const userRoutes = express.Router();
 
-// This will help us connect to the database
+// connect to databse
 const dbo = require("../db/conn");
 
-// This help convert the id from string to ObjectId for the _id.
+// This helps convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
 const bodyParser = require('body-parser')
@@ -16,7 +12,7 @@ userRoutes.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// This section will help you get a list of all the records.
+// get all users
 userRoutes.route("/user").get(function (req, res) {
   let db_connect = dbo.getDb("Famify");
   db_connect
@@ -28,17 +24,51 @@ userRoutes.route("/user").get(function (req, res) {
     });
 });
 
-// This section will help you get a single record by id
-userRoutes.route("/user/:id").get(function (req, res) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  db_connect.collection("user").findOne(myquery, function (err, result) {
-    if (err) throw err;
-    res.json(result);
-  });
-});
+// get one user by id
+userRoutes.route("/user/:id")
 
-// This section will help you create a new record.
+  // get one user
+  .get(function (req, res) {
+    let db_connect = dbo.getDb();
+    let myquery = { _id: ObjectId(req.params.id) };
+    db_connect.collection("user").findOne(myquery, function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+  })
+
+  // update user
+  .put(function (req, response) {
+    let db_connect = dbo.getDb();
+    let myquery = { _id: ObjectId(req.params.id) };
+    let newvalues = {
+      $set: {
+        name: req.body.name,
+        email: req.body.email,
+        family: req.body.family,
+      },
+    };
+    db_connect
+      .collection("users")
+      .updateOne(myquery, newvalues, function (err, res) {
+        if (err) throw err;
+        console.log("1 document updated");
+        response.json(res);
+      });
+  })
+
+  // delete user
+  .delete((req, response) => {
+    let db_connect = dbo.getDb();
+    let myquery = { _id: ObjectId(req.params.id) };
+    db_connect.collection("users").deleteOne(myquery, function (err, obj) {
+      if (err) throw err;
+      console.log("1 document deleted");
+      response.json(obj);
+    });
+  });
+
+// create new user and add to database
 userRoutes.route("/user/add").post(function (req, response) {
   let db_connect = dbo.getDb();
   let myobj = {
@@ -50,37 +80,6 @@ userRoutes.route("/user/add").post(function (req, response) {
   db_connect.collection("users").insertOne(myobj, function (err, res) {
     if (err) throw err;
     response.json(res);
-  });
-});
-
-// This section will help you update a record by id.
-userRoutes.route("/update/:id").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  let newvalues = {
-    $set: {
-      name: req.body.name,
-      position: req.body.email,
-      level: req.body.family,
-    },
-  };
-  db_connect
-    .collection("users")
-    .updateOne(myquery, newvalues, function (err, res) {
-      if (err) throw err;
-      console.log("1 document updated");
-      response.json(res);
-    });
-});
-
-// This section will help you delete a record
-userRoutes.route("/:id").delete((req, response) => {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  db_connect.collection("users").deleteOne(myquery, function (err, obj) {
-    if (err) throw err;
-    console.log("1 document deleted");
-    response.json(obj);
   });
 });
 
