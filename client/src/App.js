285c08c1ae6902c054bf6970bcf343a06d508cca
Kadin Sayani <kadinsayani8@gmail.com>
2022-11-14 13:57:20 -0700

    added routing and homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
-
-// We use Route in order to define the different routes of our application
-import { Route, Routes } from "react-router-dom";
+import { React, useState } from "react";
+// import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login.js";
 import Register from "./components/Register.js";
+import Homepage from "./components/Homepage.js";
 
 const App = () => {
+  const [user, setLoginUser] = useState({});
   return (
     <div>
-      <Register />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {user && user._id ? <Homepage /> : <Login />} <Homepage />
+          </Route>
+          <Route path="/Login">
+            <Login setLoginUser={setLoginUser} />
+          </Route>
+          <Route path="/Register">
+            <Register />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
