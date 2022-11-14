d620ecfb8de4794595fe2a4ae57080be95e24fbb
Kadin Sayani <kadinsayani8@gmail.com>
2022-11-14 13:51:43 -0700

    added state management for register

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default function Login() {
-  return (
-    <div>
-      <h1>Register</h1>
-      <form>
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" />
-        <label for="password">Password:</label>
-        <input type="text" id="password" name="password" />
-        <label for="familycode">Family join code:</label>
-        <input type="text" id="familycode" name="familycode" />
-      </form>
-      <button type="submit">Submit</button>
-    </div>
-  );
-}
+const Register = () => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    const { username, value } = e.target;
+    setUser({ ...user, [username]: value });
+  };
+  const register = () => {
+    const { username, password } = user;
+    if (username && password) {
+      axios.post("/user/add", user).then((res) => {
+        console.log(res.data);
+      });
+    } else {
+      alert("Please enter username and password");
+    }
+    return (
+      <div>
+        <h1>Register</h1>
+        <form>
+          <label for="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={user.username}
+            onChange={handleChange}
+          />
+          <label for="password">Password:</label>
+          <input
+            type="text"
+            id="password"
+            name="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+          <label for="familycode">Family join code:</label>
+          <input type="text" id="familycode" name="familycode" />
+        </form>
+        <button type="submit" onClick={register}>
+          Register
+        </button>
+      </div>
+    );
+  };
+};
+
+export default Register;
