bc59d8d4f0008e560594d6005b7b92f1315de2b6
Kadin Sayani <kadinsayani8@gmail.com>
2022-11-14 14:22:31 -0700

    updated server.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,86 +1,86 @@
-const express = require("express");
-const userRoutes = express.Router();
+// const express = require("express");
+// const userRoutes = express.Router();
 
-// connect to databse
-const dbo = require("../db/conn");
+// // connect to databse
+// const dbo = require("../db/conn");
 
-// This helps convert the id from string to ObjectId for the _id.
-const ObjectId = require("mongodb").ObjectId;
+// // This helps convert the id from string to ObjectId for the _id.
+// const ObjectId = require("mongodb").ObjectId;
 
-const bodyParser = require('body-parser')
-userRoutes.use(bodyParser.urlencoded({
-  extended: true
-}));
+// const bodyParser = require('body-parser')
+// userRoutes.use(bodyParser.urlencoded({
+//   extended: true
+// }));
 
-// get all users
-userRoutes.route("/user").get(function (req, res) {
-  let db_connect = dbo.getDb("Famify");
-  db_connect
-    .collection("users")
-    .find({})
-    .toArray(function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
-});
+// // get all users
+// userRoutes.route("/user").get(function (req, res) {
+//   let db_connect = dbo.getDb("Famify");
+//   db_connect
+//     .collection("users")
+//     .find({})
+//     .toArray(function (err, result) {
+//       if (err) throw err;
+//       res.json(result);
+//     });
+// });
 
-// get one user by id
-userRoutes.route("/user/:id")
+// // get one user by id
+// userRoutes.route("/user/:id")
 
-  // get one user
-  .get(function (req, res) {
-    let db_connect = dbo.getDb();
-    let myquery = { _id: ObjectId(req.params.id) };
-    db_connect.collection("user").findOne(myquery, function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
-  })
+//   // get one user
+//   .get(function (req, res) {
+//     let db_connect = dbo.getDb();
+//     let myquery = { _id: ObjectId(req.params.id) };
+//     db_connect.collection("user").findOne(myquery, function (err, result) {
+//       if (err) throw err;
+//       res.json(result);
+//     });
+//   })
 
-  // update user
-  .put(function (req, response) {
-    let db_connect = dbo.getDb();
-    let myquery = { _id: ObjectId(req.params.id) };
-    let newvalues = {
-      $set: {
-        name: req.body.name,
-        email: req.body.email,
-        family: req.body.family,
-      },
-    };
-    db_connect
-      .collection("users")
-      .updateOne(myquery, newvalues, function (err, res) {
-        if (err) throw err;
-        console.log("1 document updated");
-        response.json(res);
-      });
-  })
+//   // update user
+//   .put(function (req, response) {
+//     let db_connect = dbo.getDb();
+//     let myquery = { _id: ObjectId(req.params.id) };
+//     let newvalues = {
+//       $set: {
+//         name: req.body.name,
+//         email: req.body.email,
+//         family: req.body.family,
+//       },
+//     };
+//     db_connect
+//       .collection("users")
+//       .updateOne(myquery, newvalues, function (err, res) {
+//         if (err) throw err;
+//         console.log("1 document updated");
+//         response.json(res);
+//       });
+//   })
 
-  // delete user
-  .delete((req, response) => {
-    let db_connect = dbo.getDb();
-    let myquery = { _id: ObjectId(req.params.id) };
-    db_connect.collection("users").deleteOne(myquery, function (err, obj) {
-      if (err) throw err;
-      console.log("1 document deleted");
-      response.json(obj);
-    });
-  });
+//   // delete user
+//   .delete((req, response) => {
+//     let db_connect = dbo.getDb();
+//     let myquery = { _id: ObjectId(req.params.id) };
+//     db_connect.collection("users").deleteOne(myquery, function (err, obj) {
+//       if (err) throw err;
+//       console.log("1 document deleted");
+//       response.json(obj);
+//     });
+//   });
 
-// create new user and add to database
-userRoutes.route("/user/add").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myobj = {
-    name: req.body.name,
-    email: req.body.email,
-    family: req.body.family,
-  };
+// // create new user and add to database
+// userRoutes.route("/user/add").post(function (req, response) {
+//   let db_connect = dbo.getDb();
+//   let myobj = {
+//     name: req.body.name,
+//     email: req.body.email,
+//     family: req.body.family,
+//   };
 
-  db_connect.collection("users").insertOne(myobj, function (err, res) {
-    if (err) throw err;
-    response.json(res);
-  });
-});
+//   db_connect.collection("users").insertOne(myobj, function (err, res) {
+//     if (err) throw err;
+//     response.json(res);
+//   });
+// });
 
-module.exports = userRoutes;
+// module.exports = userRoutes;
--- server/server.js
+++ server/server.js
@@ -1,19 +1,83 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
+// const port = process.env.PORT || 5000;
+// app.use(cors());
+// app.use(express.json());
+// app.use(require("./routes/users"));
+// app.use(require("./routes/families"));
+// // get driver connection
+// const dbo = require("./db/conn");
+
+// app.listen(port, () => {
+//   // perform a database connection when server starts
+//   dbo.connectToServer(function (err) {
+//     if (err) console.error(err);
+//   });
+//   console.log(`Server is running on port: ${port}`);
+// });
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
 app.use(express.json());
-app.use(require("./routes/users"));
-app.use(require("./routes/families"));
-// get driver connection
-const dbo = require("./db/conn");
+app.use(express.urlencoded());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+mongoose.connect("mongodb://localhost:27017/famify", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}); () => {
+  console.log("Connected to MongoDB");
+}
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password : String,
+})
+
+const User = new mongoose.model("User", userSchema);
+
+app.post("/Login", (req, res) => {
+  const {username, password} = req.body;
+  User.findOne({username:username}, (err, user) => {
+    if (user) {
+      if (user.password === password) {
+        res.send("Success");
+      } else {
+        res.send("Wrong password");
+      }
+    } else {
+      res.send("User not found");
+    }
+  }) 
+});
+
+app.post("/Register", (req, res) => {
+  console.log(req.body);
+  const {username, password} = req.body;
+  User.findOne({username:username}, (err, user) => {
+    if (user)
+    {
+      res.send("User already exists");
+    }
+    else {
+      const user = new User({ username, password })
+      user.save(err=>{
+        if (err){
+          res.send(err)
+        } else {
+          res.send("Success");
+        }
+      )
+    }
+  })
+});
 
 app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-  });
   console.log(`Server is running on port: ${port}`);
 });
