5876d6bda96ff8387550ed5d7c813e3eadf598cf
Samiha Hoque <samiha.hoque@ucalgary.ca>
2022-12-04 14:06:42 -0700

    fixed pacakage issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login.js";
