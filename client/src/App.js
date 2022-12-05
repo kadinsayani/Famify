1b2eb17c58c175a7f8d59cab1e22fc2101cb5634
Kadin Sayani <kadinsayani8@gmail.com>
2022-12-04 18:14:27 -0700

    initial navbar stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
 import React, { useState } from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login.js";
 import Register from "./components/Register.js";
 import Homepage from "./components/Homepage.js";
 import TaskList from "./components/TaskList.js";
 import FamFeed from "./components/FamFeed";
+import Navbar from "./components/Navbar.js";
 
 const App = () => {
   const [user, setLoginUser] = useState({});
   return (
     <div>
-      <Routes>
+      <Router>
+        <Navbar>
+          <Route>
+            <Route path="" />
+          </Route>
+        </Navbar>
+      </Router>
+      {/* <Routes>
+        <Navbar />
+        <Route path="/homepage" element={<Homepage />} />
+      </Routes> */}
+
+      {/* <Routes>
         <Route path="/" element={user && user._id ? <Homepage /> : <Login />} />
         <Route path="/homepage" element={<Homepage />} />
         <Route path="/login" element={<Login setLoginUser={setLoginUser} />} />
         <Route path="/register" element={<Register />} />
         <Route path="/tasklist" element={<TaskList />} />
         <Route path="/famfeed" element={<FamFeed />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 };
