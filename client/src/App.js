5f12c8f3564a31d291bc974d750d2057077f09ec
Kadin Sayani <kadinsayani8@gmail.com>
2022-12-05 23:37:34 -0700

    initial navbar stuff

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Login from "./components/Login.js";
 import Register from "./components/Register.js";
 import Homepage from "./components/Homepage.js";
 import TaskList from "./components/TaskList.js";
-import FamFeed from "./components/FamFeed";
+import FamFeed from "./components/FamFeed.js";
 import Navbar from "./components/Navbar.js";
 
 const App = () => {
@@ -16,23 +16,21 @@ const App = () => {
       <Router>
         <Navbar>
           <Route>
-            <Route path="" />
+            <Route
+              path="/"
+              element={user && user._id ? <Homepage /> : <Login />}
+            />
+            <Route path="/homepage" element={<Homepage />} />
+            <Route
+              path="/login"
+              element={<Login setLoginUser={setLoginUser} />}
+            />
+            <Route path="/register" element={<Register />} />
+            <Route path="/tasklist" element={<TaskList />} />
+            <Route path="/famfeed" element={<FamFeed />} />
           </Route>
         </Navbar>
       </Router>
-      {/* <Routes>
-        <Navbar />
-        <Route path="/homepage" element={<Homepage />} />
-      </Routes> */}
-
-      {/* <Routes>
-        <Route path="/" element={user && user._id ? <Homepage /> : <Login />} />
-        <Route path="/homepage" element={<Homepage />} />
-        <Route path="/login" element={<Login setLoginUser={setLoginUser} />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/tasklist" element={<TaskList />} />
-        <Route path="/famfeed" element={<FamFeed />} />
-      </Routes> */}
     </div>
   );
 };
