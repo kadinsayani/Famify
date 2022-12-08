22e2346d48dcca63b4df475729891fc2d56c2536
Kadin Sayani <kadinsayani8@gmail.com>
2022-12-07 17:50:25 -0700

    removed unused imports

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,29 +1,16 @@
-import React, { useState } from "react";
-import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
+import React from "react";
 import { Link } from "react-router-dom";
 import { NavbarData } from "./NavbarData";
 import "./Navbar.css";
 import { IconContext } from "react-icons";
 
 function Navbar() {
-  const [navbar, setNavbar] = useState(false);
-  // const showNavbar = () => setNavbar(!navbar);
   return (
     <>
       <IconContext.Provider value={{ color: "#0eb2fc" }}>
-        <div className="navbar">
-          {/* <Link to="#" className="menu-bars">
-            <FaIcons.FaBars onClick={showNavbar} />
-          </Link> */}
-        </div>
+        <div className="navbar"></div>
         <nav className="nav-menu">
           <ul className="nav-menu-items">
-            {/* <li className="navbar-toggle">
-              <Link to="#" className="menu-bars">
-                <AiIcons.AiOutlineClose />
-              </Link>
-            </li> */}
             {NavbarData.map((item, index) => {
               return (
                 <li key={index} className={item.cName}>
