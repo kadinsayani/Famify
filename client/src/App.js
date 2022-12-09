fd083599f25bbd7bf40b58e34e7e084145cb6a6e
Samiha Hoque <samiha.hoque@ucalgary.ca>
2022-12-09 01:38:09 -0700

    added styling for login page

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <>
       <Router>
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
           <Route
             path="/"
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import "./Login.css";
+import Logo from "./famify_logo.png";
 
 const Login = ({ setLoginUser }) => {
   const [user, setUser] = useState({
@@ -24,46 +26,66 @@ const Login = ({ setLoginUser }) => {
   };
 
   return (
-    <div>
+  <div>
+
+    <div className="showcase"><img src={Logo}/></div>
+    
+    <div className="left">
       <h1>Login</h1>
+      
+      <div className="box-two">
 
-      <form>
-        <label htmlFor="username">Username: </label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          value={user.username}
-          onChange={handleChange}
-        />
+        <form>
+          <label htmlFor="username">Username </label>
+          <br></br>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Enter your username"
+            value={user.username}
+            onChange={handleChange}
+          />
+          <p></p>
+          <br></br>
 
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={user.password}
-          onChange={handleChange}
-        />
+          <label htmlFor="password">Password</label>
+          <br></br>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="*******"
+            value={user.password}
+            onChange={handleChange}
+          />
+        
+          <p></p>
+          <br></br>
 
-        <label htmlFor="password">Family:</label>
-        <input
-          type="text"
-          id="family"
-          name="family"
-          value={user.family}
-          onChange={handleChange}
-        />
-      </form>
+          <label htmlFor="password">Family Code</label>
+          <br></br>
+          <input
+            type="text"
+            id="family"
+            name="family"
+            placeholder="Enter your family join code"
+            value={user.family}
+            onChange={handleChange}
+          />
+        </form>
 
-      <button type="submit" onClick={useNavigate("/register")}>
-        Don't have an account?
-      </button>
+        <p></p>
+        <button className="login-button" type="submit" onClick={login}>
+          Login
+        </button>
+        <button className="register-button" type="submit" onClick={useNavigate("/register")}>
+          Don't have an account?
+        </button>
+      </div>
 
-      <button type="submit" onClick={login}>
-        Login
-      </button>
     </div>
+  </div>
   );
 };
 
