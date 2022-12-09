5b0b332c48328b2652a1aff7294c8fe465034a05
Kadin Sayani <kadinsayani8@gmail.com>
2022-12-09 16:58:23 -0700

    changed back to previous state (post.js)

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,17 +1,7 @@
 import React from "react";
 
 function Post({ posts }) {
-  if (!posts) {
-    return null;
-  }
-
-  if (typeof posts === "object" && !Array.isArray(posts)) {
-    posts = Object.entries(posts);
-  }
-
-  return posts.map((post) => (
-    <div key={post.id || post.timestamp}>{post.content}</div>
-  ));
+  return posts.map((post, index) => <div key={post._id}>{post.content}</div>);
 }
 
 export default Post;
