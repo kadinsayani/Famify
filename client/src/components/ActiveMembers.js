6aefd5ec930f8ea8dee256b3e3760a232dfcab49
Kadin Sayani <kadinsayani8@gmail.com>
2022-12-11 14:58:45 -0700

    fixed import issue

--- client/src/components/ActiveMembers.js
+++ client/src/components/ActiveMembers.js
@@ -2,31 +2,19 @@ import React from "react";
 import "./ActiveMembers.css";
 import { BsPersonCircle } from "react-icons/bs";
 import { IconContext } from "react-icons";
-import { ActiveMembersData } from "./ActiveMembersData";
 
 function ActiveMembers() {
   return (
     <>
-      <IconContext.Provider value={{ color: "#0eb2fc", size: '30px'}}>
+      <IconContext.Provider value={{ color: "#0eb2fc", size: "30px" }}>
         <div className="activemembers"></div>
         <nav className="members-menu">
-        <BsPersonCircle />
-          <ul className="members-menu-items">
-
-            {ActiveMembersData.map((item, index) => {
-              return (
-                  <li key={index} className={item.cName}>
-                          <span>{item.icon}</span>
-                          <span>{item.title}</span>
-                          <span>{item.status}</span>
-                      </li>
-              );
-            })}
-          </ul>
+          <BsPersonCircle />
+          <ul className="members-menu-items"></ul>
         </nav>
       </IconContext.Provider>
     </>
   );
 }
 
-export default ActiveMembers; 
+export default ActiveMembers;
