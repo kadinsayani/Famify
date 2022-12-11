aea51c3d62c3ab7465900eb6da45a040ef50b772
kkluddu <60626058+kkluddu@users.noreply.github.com>
2022-12-11 13:17:03 -0700

    Update ActiveMembersData.js

--- client/src/components/ActiveMembersData.js
+++ client/src/components/ActiveMembersData.js
@@ -4,21 +4,24 @@ import { IconContext } from "react-icons/lib";
 export const ActiveMembersData = [
   {
     title: "Who is in your Fam",
-    cName: "name-text",
+    cName: "name-text0",
   },
   {
     title: "Kiyana Smith",
     icon: <BsPersonCircle />,
+    status: "Online",
     cName: "name-text",
   },
   {
     title: "Ryan Smith",
     icon: <BsPersonCircle />,
+    status: "Online",
     cName: "name-text",
   },
   {
     title: "Sarah Smith",
     icon: <BsPersonCircle />,
-    cName: "name-text",
+    status: "Offline",
+    cName: "name-text2",
   },
 ];
