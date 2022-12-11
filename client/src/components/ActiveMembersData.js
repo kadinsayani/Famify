c0dbbdfb2f8c7f8abd2a10dbec8e49c772d642e7
kkluddu <60626058+kkluddu@users.noreply.github.com>
2022-12-11 08:44:25 -0700

    Create ActiveMembersData.js

---
+++ client/src/components/ActiveMembersData.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BsPersonCircle } from "react-icons/bs";
+import { IconContext } from "react-icons/lib";
+export const ActiveMembersData = [
+  {
+    title: "Who is in your Fam",
+    cName: "name-text",
+  },
+  {
+    title: "Kiyana Smith",
+    icon: <BsPersonCircle />,
+    cName: "name-text",
+  },
+  {
+    title: "Ryan Smith",
+    icon: <BsPersonCircle />,
+    cName: "name-text",
+  },
+  {
+    title: "Sarah Smith",
+    icon: <BsPersonCircle />,
+    cName: "name-text",
+  },
+];
