8a221e9cf221387fabc713f3c5037d0b5d3f8054
Kadin Sayani <kadinsayani8@gmail.com>
2022-12-13 21:00:47 -0700

    intermediate groceries

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,40 +1,17 @@
-import React, { useState } from "react";
-import ListForm from "./ListForm";
+import React from "react";
 import { RiCloseCircleLine } from "react-icons/ri";
-import { TiEdit } from "react-icons/ti";
 import "./List.css";
 
-function List({ lists, completeList, removeList, updateList }) {
-  const [edit, setEdit] = useState({
-    id: null,
-    value: "",
-  });
-  const submitUpdate = (value) => {
-    updateList(edit.id, value);
-    setEdit({
-      id: null,
-      value: "",
-    });
-  };
-  if (edit.id) {
-    return <ListForm edit={edit} onSubmit={submitUpdate} />;
-  }
-  return lists.map((list, index) => (
+function List({ items, removeItems }) {
+  return items.map((list, index) => (
     <div
       className={list.isComplete ? "list-row complete" : "list-row"}
       key={index}
     >
-      <div key={list.id} onClick={() => completeList(list.id)}>
-        {list.text}
-      </div>
       <div className="icons">
         <RiCloseCircleLine
-          onClick={() => removeList(list.id)}
+          onClick={() => removeItems(list.id)}
           className="delete-icone"
-        />
-        <TiEdit
-          onClick={() => setEdit({ id: list.id, value: list.text })}
-          className="edit-icone"
         />
       </div>
     </div>
