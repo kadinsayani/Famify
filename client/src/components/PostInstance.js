5218426e6f50a570391064c2a2b93361833af57a
Leinard Grande <leinard.grande@ucalgary.ca>
2022-12-13 20:51:09 -0700

    FamFeed fixes, reversed posts order

---
+++ client/src/components/PostInstance.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./FamFeed.css";
+import { BsPersonCircle } from "react-icons/bs";
+
+function PostInstance(props) {
+
+  return (
+  <div className="postDiv" key={props.key}>
+
+    <div className="userInfoDiv">
+      <div className="userInfoImg">
+        <BsPersonCircle color="#0eb2fc" size="30px" />
+      </div>
+      <span className="username">{props.user}</span>
+      <span className="userInfoDateTime">{props.time} on {props.date}</span>
+    </div>
+
+    <div className="postContentDiv">
+
+      {props.content}
+    
+    </div>
+
+  </div>
+  )
+
+}
+
+export default PostInstance;
